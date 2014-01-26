e239ccbbf3beecb0aa0d729f6ba6f5c59f11dfc5
Mike Adeleke <madeleke13@gmail.com>
2014-01-25 19:30:40 -0600

    started on the js

---
+++ main.js
@@ -0,0 +1,38 @@
+var random_number = Math.random(1..10)
+var x = prompt("Pick a number between 1 and 10.")
+function(x) {
+  if random_number == x
+    alert("Amazing! Are you cheating?") 
+  else if random_number > x
+    alert("Too hot!")
+  else if random_number < x
+    alert("Too cold!")
+  else
+    alert("Please enter a number between 1 and 10.")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// On page load, Javascript should pick a number at random that user will have to guess.
+// The user should be prompted to enter a guess.
+// The user should get feedback about their guess -- if it was too high, too low, or just right. Initially, don't worry about telling users if they're getting "hotter" or "colder", just tell them if there previous guess was "hot" or "cold".
+// For your initial version, feel free to use the built-in "prompt()" function to get user guesses, and the "alert()" function to give feedback on the user guess.
+// For the final version of your project, you'll need to create an HTML-based interface for getting user inputs and giving feedback on guesses.
+// Your app should ensure that users provide valid inputs. For instance, if a user submits a spaces, a blank guess, or letters, they shoudl receive feedback that they need to input a number.
+// The app should feature a button that allows the user to start a new game, without making additional calls to the server
+// Make sure to use version control as you're working on this project, committing early and often.
