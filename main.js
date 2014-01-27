5a370bb632a5f566502eb15b536af768462ce567
Mike Adeleke <madeleke13@gmail.com>
2014-01-27 12:24:32 -0600

    added base styling a four js functions

--- main.js
+++ main.js
@@ -1,27 +1,41 @@
-var randomnumber = Math.floor(Math.random()*11);
-var x = prompt("Pick a number between 1 and 10.");
-var number = function(x) {
-  if (randomnumber == x)
-    alert("Amazing! Are you cheating ;)"); 
-  else if (randomnumber > x)
-    alert("Too hot!");
-  else if (randomnumber < x)
-    alert("Too cold!");
-  else
-    alert("Please enter a number between 1 and 10.");
-};
+$(document).ready(function() {
+  var randomnumber = Math.floor(Math.random()*11);
+  var x = prompt("Pick a number between 1 and 10.");
+  
+  function getGuess() {
+    $("#submit").click(game);
+    $("guess").keydown(function() {
+      // some code
+    })
+  };
 
-console.log(number(x));
+  getGuess();
+
+  function game() {
+    var number = function(x) {
+      if (randomnumber == x)
+        console.log("Amazing! Are you cheating ;)"); 
+      else if (randomnumber > x)
+        console.log("Too hot!");
+      else if (randomnumber < x)
+        console.log("Too cold!");
+      else
+        console.log("Please enter a number between 1 and 10.");
+    };
+  };
+
+  function newGame() {
+    // some code
+  }
+
+})
 
 
 
 
 
-// On page load, Javascript should pick a number at random that user will have to guess.
-// The user should be prompted to enter a guess.
-// The user should get feedback about their guess -- if it was too high, too low, or just right. Initially, don't worry about telling users if they're getting "hotter" or "colder", just tell them if there previous guess was "hot" or "cold".
-// For your initial version, feel free to use the built-in "prompt()" function to get user guesses, and the "alert()" function to give feedback on the user guess.
-// For the final version of your project, you'll need to create an HTML-based interface for getting user inputs and giving feedback on guesses.
-// Your app should ensure that users provide valid inputs. For instance, if a user submits a spaces, a blank guess, or letters, they shoudl receive feedback that they need to input a number.
-// The app should feature a button that allows the user to start a new game, without making additional calls to the server
-// Make sure to use version control as you're working on this project, committing early and often.
+// set the variables
+// get the guess
+// start the game
+// new game
+
