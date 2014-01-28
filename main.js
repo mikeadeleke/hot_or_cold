00e2f2883b3b96fd9e5437703bea23b595be1526
Mike Adeleke <madeleke13@gmail.com>
2014-01-27 22:45:15 -0600

    added functionality for new game

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
 $(document).ready(function() {
-  var randomnumber = Math.floor(Math.random()*11);
-  var x = prompt("Pick a number between 1 and 10.");
+  var randomnumber = Math.floor(Math.random()*101);
   
   function getGuess() {
-    $("#submit").click(game);
-    $("guess").keydown(function() {
-      // some code
-    })
-  };
+    $("#submit").click(function() {
+      game();
+    });
+    $("#guess").keydown(function(e){
+      if(e.which == 13) {
+        game();
+      }
+    });
+  }
 
   getGuess();
 
   function game() {
-    var number = function(x) {
-      if (randomnumber == x)
-        console.log("Amazing! Are you cheating ;)"); 
-      else if (randomnumber > x)
-        console.log("Too hot!");
-      else if (randomnumber < x)
-        console.log("Too cold!");
-      else
-        console.log("Please enter a number between 1 and 10.");
-    };
-  };
-
-  function newGame() {
-    // some code
+    var x = document.getElementById("guess").value;
+    if (randomnumber == x)
+      console.log("Amazing! Are you cheating ;)"); 
+    else if (randomnumber > x)
+      console.log("Too hot!");
+    else if (randomnumber < x)
+      console.log("Too cold!");
+    else
+      console.log("Please enter a number between 1 and 100.");
   }
+  
+  $("#button").click(function() {
+    randomnumber = Math.floor(Math.random()*101);
+    var x = document.getElementById('guess').value = "";
+  });
+    
+  
+  $("#cheat").click(function() {
+    console.log(randomnumber);
+  });
 
-})
 
-
-
-
-
-// set the variables
-// get the guess
-// start the game
-// new game
-
+});
